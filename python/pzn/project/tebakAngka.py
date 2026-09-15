029d9372ee14af3803d275670ca9b5d7e872d28c
radith <dnradith@gmail.com>
2026-09-15 21:06:34 +0700

    update tebak angka

--- python/pzn/project/tebakAngka.py
+++ python/pzn/project/tebakAngka.py
@@ -2,6 +2,27 @@ def app_tebak_angka():
     import random
 
     angka_random = random.randint(1, 100)
+    max_percobaan = 3
+    percobaan = 0
+    while percobaan < max_percobaan:
+        try:
+            tebakan = int(input("masukkan angka : "))
+        except ValueError:
+            print("masukkan input yang valid!")
+            continue
+
+        percobaan += 1
+
+        if tebakan == angka_random:
+            print("tebakanmu benar!")
+            break
+        elif tebakan > angka_random:
+            print("angkamu terlalu besar!")
+        elif tebakan < angka_random:
+            print("angkamu terlalu kecil!")
+    else:
+        print("percobaan habis, kamu gagal!")
+        print(f"angka sebenarnya adalah {angka_random}")
 
 
 def app_menu():
@@ -15,6 +36,8 @@ def app_menu():
         app_tebak_angka()
     elif pilihan == "2":
         print("selamat tinggal")
-        break
     else:
         print("pilihan tidak valid")
+
+
+app_menu()
